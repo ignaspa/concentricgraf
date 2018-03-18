96b21e6152bcd611dcfbf91b3ca45e9c7852f432
ignaspa <ignaspan@gmail.com>
2018-03-18 02:19:39 -0400

    almost done, bugs tho

--- graph.py
+++ graph.py
@@ -5,7 +5,7 @@ from typing import *
 
 
 class CircleGraph:
-    def __init__(self, categories: Dict[Any, List[Any]], elements: List) -> None:
+    def __init__(self, categories: Dict[Any, List[Any]], elements: List)->None:
         """
         """
         self.cat = categories
@@ -20,6 +20,7 @@ class CircleGraph:
         white = (255,255,255)
         pink = (255,200,200)
         darkBlue = (0,0,128)
+        black = (0,0,0)
         thickness = 10
         #screen measurements and measurement of square that circle will be w/in
         screen_w = (self.num_cat * thickness * 2) + 10
@@ -29,15 +30,38 @@ class CircleGraph:
         ICR = (len(self.el) * 4) / (2 * math.pi)
 
         #degrees per slice
-        dps = (math.pi * 2) / self.num_el
+        dps = (math.pi * 2) / (self.num_el + 1)
 
 
         #actual window
         pygame.init()
         screen = pygame.display.set_mode((screen_w, screen_h))
-        pygame.display.set_caption("hello world")
+        pygame.display.set_caption("Circle Graph!")
         clock = pygame.time.Clock()
         screen.fill(white)
+        #distance from center
+        min = ICR
+        #enclosing box coordinates
+        eb = square/2 - min
+        #each ring
+        for w in self.cat:
+
+            #each gene in the ring
+            for k in range(len(self.el)):
+                if self.el[k] in self.cat[w]:
+                    color = darkBlue
+                else:
+                    color = pink
+                #draw slice
+                pygame.draw.arc(screen, color,
+                                (eb,eb,min*2,min*2),
+                                dps*k, (dps * (k + 1)) - 1, thickness)
+                #draw slice border
+                pygame.draw.arc(screen, black,
+                                (eb,eb,min*2,min*2),
+                                (dps * (k + 1)) - 1, (dps * (k + 1)), thickness)
+                min = min + thickness
+                eb = eb - thickness
         pygame.display.update()
         pygame.display.flip()
 
@@ -49,26 +73,12 @@ class CircleGraph:
 
         pygame.quit()
 
-        enclosingBox = square/2 - ICR
-        #each ring
-        for w in range(len(self.cat)):
-            degree = 0
-
-            #each gene in the ring
-            for k in range(len(self.num_el)):
-                if self.num_el[k] in self.cat[w]:
-                    color = darkBlue
-                else:
-                    color = pink
-
-                pygame.draw.arc(screen, color, (enclosingBox,enclosingBox,square,square), dps*k,
-                        dps * (k + 1), thickness)
-                enclosingBox = enclosingBox - 10
 
 
 
 
 
 
-bob = CircleGraph({"bob":3},[0])
-bob.draw_graph(300,300)
+
+bob = CircleGraph({"bob":[3], "tom":[7, 3]},[3])
+bob.draw_graph()
